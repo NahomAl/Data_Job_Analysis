00398ad18a726fb654e4e36bebdbcd04faddfd6b
Nahom Alemu <128564728+NahomAl@users.noreply.github.com>
2025-08-02 01:47:17 +0300

    Add SQL query to identify top-paying remote data jobs

---
+++ project_sql/top_paying_jobs.sql
@@ -0,0 +1,22 @@
+/*
+ What are the top-paying data related jobs?
+ - Identifying the top 20 highest-paying Data roles that are available remotely
+ - Focuses on job postings with specified salaries (remove nulls)
+ - Include company names of top 20 roles
+ - Why? Highlight the top-paying opportunities for Data professionals, offering insights into employment options and location flexibility.
+ */
+SELECT job_id,
+    job_title,
+    company_dim.name AS company_name,
+    job_location,
+    job_country,
+    job_schedule_type,
+    job_posted_date,
+    salary_year_avg AS anual_salary
+FROM job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE job_title_short LIKE '%Data%'
+    AND job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+ORDER BY anual_salary DESC
+LIMIT 20;
